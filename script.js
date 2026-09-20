6d0554c5477baa49d46626ac6b8988bc42f6cd85
Lizeth Natalia Gutierrez Piña <pepeevaristogarcia98@gmail.com>
2026-09-20 00:05:13 -0400

    Actualizar mensaje de la carta de amor

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const cartaModal = document.getElementById('cartaModal');
 
   // Para rellenar la carta modal con el mismo contenido
-  const tituloCarta = localStorage.getItem("tituloCarta") || "Para mi cielo 🌌";
-  const mensajeCarta = localStorage.getItem("mensajeCarta") || "En tu mirada encuentro mi universo.<br>Gracias por llenar mis días de luz, cariño y magia.";
-  const firmaCarta = localStorage.getItem("firmaCarta") || "Con amor eterno,<br><strong>Tu persona especial</strong>";
+  const tituloCarta = localStorage.getItem("tituloCarta") || "Feliz día del amor y la amistad";
+  const mensajeCarta = localStorage.getItem("mensajeCarta") || "Eres una de las coincidencias más lindas que me ha dado la vida.<br>Espero seguir a tu lado durante mucho tiempo<br>y compartir juntos por primera vez todo lo que falta. <br>La vida es muy corta para no disfrutarla a tu lado.<br>Te quiero demasiado 💕";
+  const firmaCarta = localStorage.getItem("firmaCarta") || "Con todo el cariño,<br><strong>Para una persona especial</strong>";
 
   document.getElementById("tituloCarta").innerHTML = tituloCarta;
   document.getElementById("mensajeCarta").innerHTML = mensajeCarta;
